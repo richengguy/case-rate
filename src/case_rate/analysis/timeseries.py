f858283f925ba6d5f180a10eaf1692c0c75c1473
Richard Rzeszutek <richengguy@users.noreply.github.com>
2020-04-13 19:17:01 -0400

    Added slope and percent_change methods to TimeSeries.
    Just need to add a growth factor calculation and that will make it
    feature parity.

--- src/case_rate/analysis/timeseries.py
+++ src/case_rate/analysis/timeseries.py
@@ -3,7 +3,7 @@ from typing import List, Union
 
 import numpy as np
 
-from .least_squares import LeastSquares
+from .least_squares import LeastSquares, evalpoly, derivative
 from .. import filters
 from .._types import Cases, CaseTesting
 
@@ -17,6 +17,9 @@ class TimeSeries:
     ----------
     dates : list of :class:`datetime.date` instances
         list of dates the time series represents
+    daily_change : np.ndarray
+        a convenience accessor to obtain the day-to-day changes via a finite
+        difference
     '''
     def __init__(self, data: List[Datum], field: str, min_value: int = 0):
         '''
@@ -108,7 +111,7 @@ class TimeSeries:
             element in the time series.
         '''
         if log_domain:
-            x = np.log10(self._samples)
+            x = np.log(self._samples)
         else:
             x = self._samples
 
@@ -154,10 +157,83 @@ class TimeSeries:
             output[i] = ls.value(i)
 
         if log_domain:
-            output = np.power(10, output)
+            output = np.exp(output)
 
         return output
 
-            ls = LeastSquares(np.arange(i_min, i_max), x[i_min:i_max], order=2)
+    def slope(self, window: int, order: int = 1,
+              confidence: float = 0.95) -> np.ndarray:
+        '''Estimate the slope at any given point in the time series.
 
-            # TODO: Complete this
+        Parameters
+        ----------
+        window : int
+            size of the sliding window, in days, used in the slope estimate
+        order : int, optional
+            the order of the polynomial used for the slope estimation; defaults
+            to '1', which assumes the contents of the window are approximately
+            linear
+        confidence : float, optional
+            the desired confidence interval, which defaults to 95%
+
+        Returns
+        -------
+        numpy.ndarray
+            a :math:`N \\times 3`` array containing the slope and 95%
+            confidence interval, e.g. each row is ``(slope, upper_ci,
+            lower_ci)``
+        '''
+        output = np.zeros((len(self), 3))
+        regressions = self._local_regression(window, False, order)
+
+        ls: LeastSquares
+        for i, ls in enumerate(regressions):
+            weights = ls.weights
+            cv = ls.confidence(confidence)
+            t0 = np.array([i])
+
+            output[i, 0] = evalpoly(derivative(weights), t0)
+            output[i, 1] = evalpoly(derivative(weights + cv), t0)
+            output[i, 2] = evalpoly(derivative(weights - cv), t0)
+
+        return output
+
+    def percent_change(self, window: int, order: int = 1,
+                       confidence: float = 0.95) -> np.ndarray:
+        '''Estimate the day-over-day percent change.
+
+        Parameters
+        ----------
+        window : int
+            size of the sliding window, in days, used in the slope estimate
+        order : int, optional
+            the order of the polynomial used for the slope estimation; defaults
+            to '1', which assumes the contents of the window are approximately
+            linear
+        confidence : float, optional
+            the desired confidence interval, which defaults to 95%
+
+        Returns
+        -------
+        numpy.ndarray
+            a :math:`N \\times 3`` array containing the percent change and 95%
+            confidence interval, e.g. each row is ``(slope, upper_ci,
+            lower_ci)``
+        '''
+        output = np.zeros((len(self), 3))
+        regressions = self._local_regression(window, True, order)
+
+        ls: LeastSquares
+        for i, ls in enumerate(regressions):
+            weights = ls.weights
+            cv = ls.confidence(confidence)
+            t0 = np.array([i])
+
+            output[i, 0] = evalpoly(derivative(weights), t0)
+            output[i, 1] = evalpoly(derivative(weights + cv), t0)
+            output[i, 2] = evalpoly(derivative(weights - cv), t0)
+
+        # Convert into percent change by converting from the log-domain and
+        # subtracting by one.
+        output = np.exp(output) - 1
+        return output
