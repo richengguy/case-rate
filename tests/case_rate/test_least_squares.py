841afa95fddc5d6692cfaac4e24e753fb3a29853
Richard Rzeszutek <richengguy@users.noreply.github.com>
2020-04-12 20:18:48 -0400

    Updated tests for the LeastSquares class.

--- tests/case_rate/test_least_squares.py
+++ tests/case_rate/test_least_squares.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from case_rate.analysis.least_squares import LeastSquares
 
@@ -22,3 +23,16 @@ class TestLeastSquares:
         assert np.isclose(ls.weights[1], 0.3)
         assert np.isclose(ls.weights[2], 2.0)
         assert np.isclose(ls.value(0.5), 1.15)
+
+    def test_reject_insufficient_samples(self):
+        t = np.arange(2)
+        x = 2*t**2 + 0.3*t + 0.5
+        with pytest.raises(ValueError):
+            LeastSquares(t, x, order=2)
+
+    def test_reject_different_sizes(self):
+        t = np.arange(5)
+        x = 0.5*t + 1
+
+        with pytest.raises(ValueError):
+            LeastSquares(t, x[0:3])
