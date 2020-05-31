fddb5276f3f66c80954ae845cb7afa25e56a4ad6
Richard Rzeszutek <richengguy@users.noreply.github.com>
2020-05-30 20:49:44 -0400

    Finished the data import.

--- src/case_rate/cli/_helpers.py
+++ src/case_rate/cli/_helpers.py
@@ -1,5 +1,7 @@
 from typing import Optional, Tuple
 
+import click
+
 
 def parse_region_selector(region: Optional[str]) -> Tuple[Optional[str], Optional[str]]:  # noqa: E501
     '''Parses the region selection format.'''
@@ -13,3 +15,17 @@ def parse_region_selector(region: Optional[str]) -> Tuple[Optional[str], Optiona
         return (parts[0], parts[1])
     else:
         raise ValueError('Expected "<country>:<province/state>" selector.')
+
+
+def echo_item(title: str, item: str):
+    '''Echos a string of the form "**<title>:** <item>".
+
+    Parameters
+    ----------
+    title : str
+        a tile (will be bolded)
+    item : str
+        the string to display
+    '''
+    title = click.style(f'{title}:', bold=True)
+    click.echo(f'{title} {item}')
--- src/case_rate/cli/model.py
+++ src/case_rate/cli/model.py
@@ -1,39 +1,68 @@
-import click
+import pathlib
+from typing import Dict, Tuple
 
-from ._helpers import parse_region_selector
+import click
+import numpy as np
+import toml
+
+from ._helpers import echo_item, parse_region_selector
 from .. import sources
 from .. import filters
+from ..analysis import TimeSeries
 from ..storage import Storage
 
 
+_RegionKey = Tuple[str, str]
+
+
 @click.command('model')
-@click.argument('country', nargs=1)
+@click.argument('settings_path', metavar='SETTINGS',
+                type=click.Path(exists=True, file_okay=True, dir_okay=False))
 @click.pass_obj
-def command(config: dict, country: str):
-    '''Generate an SIR model for the given country/region.'''
-    input_source = sources.init_source(config['storage'], False, country,
-                                       config['sources'])
+def command(config: dict, settings_path: str):
+    '''Train an ARIMA-like RNN model on given COVID-19 case data.
 
+    The model attempts to predict the number of case count N-days into the
+    future, given current observations.  It assumes each day is a draw from an
+    unknown, non-stationary stochastic process that, over the short term,
+    appears Poisson-like.
+    '''
+    with pathlib.Path(settings_path).open() as f:
+        settings = toml.load(f)
+
+    training_params = settings['modelling']['training']
+
+    click.secho('Training Prediction Model', bold=True)
+    echo_item('Look-ahead', f'{training_params["lookahead_window"]} days')
+    echo_item('Initial Alpha', f'{training_params["initial_alpha"]}')
+    echo_item('Learning Rate', f'{training_params["learning_rate"]}')
+
+    click.secho('Features:', bold=True)
+    for feature in training_params['features']:
+        click.echo(f'  - {feature}')
+
+    click.secho('Regions:', bold=True)
+    for region in training_params['regions']:
+        click.echo(f'  - {region}')
+
+    # Get all of the training data from the data sources.
     with Storage() as storage:
-        country, province = parse_region_selector(country)
-        storage.populate(input_source)
-        cases = storage.cases(input_source, country=country,
-                              province=province)
-        cases = filters.sum_by_date(cases)
+        for region in training_params['regions']:
+            source = sources.init_source(config['storage'], False, region,
+                                         config['sources'])
+            storage.populate(source)
 
-    import matplotlib.pyplot as plt
-    import numpy as np
-    confirmed = np.array([case.confirmed for case in cases])
-    recovered = np.array([case.resolved for case in cases])
-    deceased = np.array([case.deceased for case in cases])
+        # Generate the training data.
+        training_data: Dict[_RegionKey, np.ndarray] = {}
+        for region in training_params['regions']:
+            country, province = parse_region_selector(region)
+            source = sources.select_source(region).name()
+            cases = storage.cases(source, country, province)
+            cases = filters.sum_by_date(cases)
 
-    plt.figure()
-    plt.plot(confirmed, label='Confirmed')
-    plt.plot(recovered, label='Recovered')
-    plt.plot(deceased, label='Deceased')
-    plt.legend()
+            sample = []
+            for feature in training_params['features']:
+                series = TimeSeries(cases, feature)
+                sample.append(series.daily_change)
 
-    plt.figure()
-    plt.plot(confirmed - (recovered + deceased))
-
-    plt.show()
+            training_data[(country, province)] = np.array(sample)
