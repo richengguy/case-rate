2a1c895c2f24265110d3cd97efa4ad5a177ee440
Richard Rzeszutek <richengguy@users.noreply.github.com>
2020-05-31 16:44:34 -0400

    Minor config reorganization.

--- src/case_rate/cli/model.py
+++ src/case_rate/cli/model.py
@@ -30,39 +30,43 @@ def command(config: dict, settings_path: str):
     with pathlib.Path(settings_path).open() as f:
         settings = toml.load(f)
 
-    training_params = settings['modelling']['training']
+    model_config = settings['modelling']['configuration']
+    training_config = settings['modelling']['training']
 
     click.secho('Training Prediction Model', bold=True)
-    echo_item('Look-ahead', f'{training_params["lookahead_window"]} days')
-    echo_item('Initial Alpha', f'{training_params["initial_alpha"]}')
-    echo_item('Learning Rate', f'{training_params["learning_rate"]}')
+    echo_item('Epochs', f'{training_config["epochs"]}')
+    echo_item('Learning Rate', f'{training_config["learning_rate"]}')
+    echo_item('Look-ahead', f'{model_config["lookahead_window"]} days')
+    echo_item('Initial Alpha', f'{model_config["initial_alpha"]}')
 
     click.secho('Features:', bold=True)
-    for feature in training_params['features']:
+    for feature in model_config['features']:
         click.echo(f'  - {feature}')
 
     click.secho('Regions:', bold=True)
-    for region in training_params['regions']:
+    for region in training_config['regions']:
         click.echo(f'  - {region}')
 
     # Get all of the training data from the data sources.
     with Storage() as storage:
-        for region in training_params['regions']:
+        for region in training_config['regions']:
             source = sources.init_source(config['storage'], False, region,
                                          config['sources'])
             storage.populate(source)
 
         # Generate the training data.
         training_data: Dict[_RegionKey, np.ndarray] = {}
-        for region in training_params['regions']:
+        for region in training_config['regions']:
             country, province = parse_region_selector(region)
             source = sources.select_source(region).name()
             cases = storage.cases(source, country, province)
             cases = filters.sum_by_date(cases)
 
             sample = []
-            for feature in training_params['features']:
+            for feature in model_config['features']:
                 series = TimeSeries(cases, feature)
                 sample.append(series.daily_change)
 
             training_data[(country, province)] = np.array(sample)
+
+    # Now run the training the loop.
