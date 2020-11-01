106a95072dafc84204c7ec5e7f393e2f3008e51d
Richard Rzeszutek <richengguy@users.noreply.github.com>
2020-11-01 18:29:09 -0500

    Using local regressor to estimate growth factor.

--- src/case_rate/analysis/operations.py
+++ src/case_rate/analysis/operations.py
@@ -131,14 +131,81 @@ def percent_change(ts: TimeSeries, window: int, order: int = 1,
     return output
 
 
+def _sequence_growth_factor(sequence: np.ndarray, window: int) -> np.ndarray:
+    '''Calculate the exponential growth factor for an array.
+
+    Parameters
+    ----------
+    sequence : np.ndarray
+        a 1D array containing a sequency potentially undergoing exponential
+        growth
+    window : int
+        width of the filtering window
+
+    Returns
+    -------
+    np.ndarray
+        estimated growth factor at each point in the sequence
+    '''
+    N = sequence.shape[0]
+    output = np.zeros_like(sequence, dtype=np.float)
+    output[:] = np.nan
+
+    for i in range(N):
+        i_min = max(0, i - window // 2)
+        i_max = min(N - 1, i + window // 2) + 1
+
+        x = sequence[i_min:i_max]
+        t = np.arange(i_min, i_max)
+
+        # If all values in the window are less than '0.5' then the growth rate
+        # is, by definition, '0' because nothing's happening.  The value may be
+        # smoothed, so this assumes that a value of '0.5' should be rounded
+        # down to '0'.
+        if np.all(x < 0.5):
+            output[i] = 0
+            continue
+
+        # Determine if there are any negative values.  Those may come from a
+        # number of sources, such as corrections being applied onto the time
+        # series.  If all values are negative then no growth rate can be
+        # calculated.
+        is_negative = x < 0
+        if np.all(is_negative):
+            continue
+
+        # Similarly, there must enough values to calculate a least squares fit.
+        # Since this is a linear fit, that means 3 points (can work with 2, but
+        # 3 is better).
+        valid = np.logical_not(is_negative)
+        if valid.sum() < 3:
+            continue
+
+        # Remove the invalid values and centre the window so that the centre
+        # corresponds to 'n = 0'.
+        x = x[valid]
+        t = t[valid]
+
+        # Add a small non-zero value when converting to log to allow it to work
+        # with windows where some days don't change.
+        log_x = np.log(x + 1e-10)
+
+        # The regression will find log(x[n]) = b_0 + b_1*n, where b_1 is the
+        # estimate of log(a).
+        ls = LeastSquares(t, log_x, 1)
+        output[i] = np.exp(ls.weights[1])
+
+    return output
+
+
 def growth_factor(ts: TimeSeries, window: int, order: int = 1,
                   confidence: float = 0.95) -> np.ndarray:
     '''Estimate the exponential growth factor of the time series.
 
-    The growth factor calculate assumes that the time series is describing a
-    monotonic function such as a cumulative count.  This means the finite
-    differences are always greater than or equal to zero.  Negative values are
-    filtered out prior to the calculation.
+    The growth factor is calculated from the LOESS regression provided by
+    :meth:`estimate_slope`.  The three LOESS curves, i.e. the best fit and the
+    upper and lower confidence interval curves, are used to estimate three
+    equivalent growth factor curves.
 
     Parameters
     ----------
@@ -156,74 +223,15 @@ def growth_factor(ts: TimeSeries, window: int, order: int = 1,
     -------
     numpy.ndarray
         a :math:`N \\times 3`` array containing the growth factor and 95%
-        confidence interval, e.g. each row is ``(slope, upper_ci,
-        lower_ci)``
+        confidence interval, e.g. each row is ``(slope, upper_ci, lower_ci)``
     '''
-    if order != 1:
-        raise ValueError('Order must be "1".')
-
-    N = len(ts)
-    changes = ts.daily_change
-    output = np.zeros((len(ts), 3))
-    output[:] = np.nan
-
     if window < 3:
         raise ValueError('Window size must be at least three days.')
 
-    for i in range(N):
-        # Select the filtering window.  The 'daily changes' is returned as an
-        # array, not as a TimeSeries, so this is being done directly in this
-        # loop.
-        i_min = max(0, i - window // 2)
-        i_max = min(N - 1, i + window // 2) + 1
+    slopes = estimate_slope(ts, window, order, confidence)
+    output = np.zeros_like(slopes)
 
-        x = changes[i_min:i_max]
-        t = np.arange(i_min, i_max)
-
-        '''
-        x[n] = x[0] a^n
-        log(x[n]) = log(x[0]a^n)
-                  = log(x[0]) + log(a^n)
-                  = log(x[0]) + log(a)*n
-        '''
-
-        # If all values in the window are '0' then the growth rate is, by
-        # definition, also '0' because nothing's happening.
-        if np.all(x == 0):
-            output[i, :] = 0
-            continue
-
-        # Determine if there are any negative values.  Those may come from a
-        # number of sources, such as corrections being applied onto the time
-        # series.  If all values are negative then no growth rate can be
-        # calculated.
-        is_negative = x < 0
-        if np.all(is_negative):
-            continue
-
-        # Similarly, there must enough values to calculate a least squares fit.
-        valid = np.logical_not(is_negative)
-        if valid.sum() < order+2:
-            continue
-
-        # Remove the invalid values and centre the window so that the centre
-        # corresponds to 'n = 0'.  A small non-zero value is added to make it
-        # possible to take the log of 'x' without any numerical issues.
-        x = x[valid]
-        t = t[valid]
-
-        log_x = np.log(x + 1e-10)
-
-        # Compute the least squares regression, getting the weights and
-        # confidence interval.
-        ls = LeastSquares(t, log_x, order)
-        weights = ls.weights
-        cv = ls.confidence(confidence)
-
-        # The regression will find log(x[n]) = b_0 + b_1*n, where b_1 is the
-        # estimate of log(a).
-        output[i, 0] = np.exp(weights[1])
-        output[i, 1] = np.exp(weights[1] + cv[1])
-        output[i, 2] = np.exp(weights[1] - cv[1])
+    for i in range(slopes.shape[1]):
+        output[:, i] = _sequence_growth_factor(slopes[:, i], window)
 
     return output
