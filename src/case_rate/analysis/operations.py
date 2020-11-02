7103c996942449e83520de1b4f268538bd9e4a2c
richengguy <richengguy@users.noreply.github.com>
2020-11-01 19:00:10 -0500

    Updated the growth factor calculation. (#25)

--- src/case_rate/analysis/operations.py
+++ src/case_rate/analysis/operations.py
@@ -130,9 +130,81 @@ def percent_change(ts: TimeSeries, window: int, order: int = 1,
     return output
 
 
+def _sequence_growth_factor(sequence: np.ndarray, window: int) -> np.ndarray:
+    '''Calculate the exponential growth factor for an array.
+
+    Parameters
+    ----------
+    sequence : np.ndarray
+        a 1D array containing a sequency potentially undergoing exponential
+        growth
+    window : int
+        width of the filtering window
+
+    Returns
+    -------
+    np.ndarray
+        estimated growth factor at each point in the sequence
+    '''
+    N = sequence.shape[0]
+    output = np.zeros_like(sequence, dtype=np.float)
+    output[:] = np.nan
+
+    for i in range(N):
+        i_min = max(0, i - window // 2)
+        i_max = min(N - 1, i + window // 2) + 1
+
+        x = sequence[i_min:i_max]
+        t = np.arange(i_min, i_max)
+
+        # If all values in the window are less than '0.5' then the growth rate
+        # is, by definition, '0' because nothing's happening.  The value may be
+        # smoothed, so this assumes that a value of '0.5' should be rounded
+        # down to '0'.
+        if np.all(x < 0.5):
+            output[i] = 0
+            continue
+
+        # Determine if there are any negative values.  Those may come from a
+        # number of sources, such as corrections being applied onto the time
+        # series.  If all values are negative then no growth rate can be
+        # calculated.
+        is_negative = x < 0
+        if np.all(is_negative):
+            continue
+
+        # Similarly, there must enough values to calculate a least squares fit.
+        # Since this is a linear fit, that means 3 points (can work with 2, but
+        # 3 is better).
+        valid = np.logical_not(is_negative)
+        if valid.sum() < 3:
+            continue
+
+        # Remove the invalid values and centre the window so that the centre
+        # corresponds to 'n = 0'.
+        x = x[valid]
+        t = t[valid]
+
+        # Add a small non-zero value when converting to log to allow it to work
+        # with windows where some days don't change.
+        log_x = np.log(x + 1e-10)
+
+        # The regression will find log(x[n]) = b_0 + b_1*n, where b_1 is the
+        # estimate of log(a).
+        ls = LeastSquares(t, log_x, 1)
+        output[i] = np.exp(ls.weights[1])
+
+    return output
+
+
 def growth_factor(ts: TimeSeries, window: int, order: int = 1,
                   confidence: float = 0.95) -> np.ndarray:
     '''Estimate the exponential growth factor of the time series.
+
+    The growth factor is calculated from the LOESS regression provided by
+    :meth:`estimate_slope`.  The three LOESS curves, i.e. the best fit and the
+    upper and lower confidence interval curves, are used to estimate three
+    equivalent growth factor curves.
 
     Parameters
     ----------
@@ -150,38 +222,36 @@ def growth_factor(ts: TimeSeries, window: int, order: int = 1,
     -------
     numpy.ndarray
         a :math:`N \\times 3`` array containing the growth factor and 95%
-        confidence interval, e.g. each row is ``(slope, upper_ci,
-        lower_ci)``
+        confidence interval, e.g. each row is ``(slope, upper_ci, lower_ci)``
     '''
-    N = len(ts)
-    changes = ts.daily_change
-    output = np.zeros((len(ts), 3))
-    output[:] = np.nan
-
     if window < 3:
         raise ValueError('Window size must be at least three days.')
 
-    for i in range(N):
-        i_min = max(0, i - window // 2)
-        i_max = min(N - 1, i + window // 2) + 1
+    slopes = estimate_slope(ts, window, order, confidence)
+    output = np.zeros_like(slopes)
 
-        x = changes[i_min:i_max]
-        t = np.arange(i_min, i_max)
+    for i in range(slopes.shape[1]):
+        output[:, i] = _sequence_growth_factor(slopes[:, i], window)
 
-        valid = changes[i_min:i_max] >= 1
-        if valid.sum() < order+2:
-            continue
+    # Set the upper/lower CI curves to zero if the best fit curve is zero.  The
+    # results are meaningless in this case because there wasn't enough data to
+    # do the calculation.
+    best_fit = output[:, 0]
+    confidence_intervals = output[:, 1:]
+    confidence_intervals[best_fit == 0, :] = 0
 
-        x = x[valid]
-        t = t[valid]
-        ls = LeastSquares(t, np.log(x), order)
+    # The intervals curves will have different growth factor curves, so they
+    # need to be sorted to be consistent with the idea of an upper/lower bound.
+    is_swapped = np.argmin(confidence_intervals, 1) != 0
 
-        weights = ls.weights
-        cv = ls.confidence(confidence)
-        t0 = np.array([i])
+    if np.any(is_swapped):
+        for i, swapped in enumerate(is_swapped):
+            if not swapped:
+                continue
 
-        output[i, 0] = evalpoly(derivative(weights), t0)
-        output[i, 1] = evalpoly(derivative(weights + cv), t0)
-        output[i, 2] = evalpoly(derivative(weights - cv), t0)
+            value = confidence_intervals[i, 0]
+            confidence_intervals[i, 0] = confidence_intervals[i, 1]
+            confidence_intervals[i, 1] = value
 
-    return np.exp(output)
+    output[:, 1:] = confidence_intervals
+    return output
