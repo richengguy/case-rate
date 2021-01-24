e48241452e4241d066df90259816437b6d78e679
Richard Rzeszutek <richengguy@users.noreply.github.com>
2021-01-24 16:26:39 -0500

    Can now generate templates.

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,13 +1,31 @@
 const gulp = require('gulp');
+const buildOutput = 'dist/'
 
 exports.clean = () => {
     const del = require('del');
-    return del('build')
+    return del(buildOutput)
+}
+
+function _renderHtml(template) {
+    var fn = (cb) => {
+        const exec = require('child_process').exec;
+        exec(`python render.py ${template}.jinja2 ${buildOutput}${template}.html`,
+            (err, stdout, stderr) => {
+                if (stdout.length > 0) { console.log(stdout); }
+                if (stderr.length > 0) { console.log(stderr); }
+                cb(err);
+            }
+        );
+    };
+    fn.displayName = template;
+    return fn;
 }
 
 exports.css = () => {
     const postcss = require('gulp-postcss');
     return gulp.src('css/*.css')
         .pipe(postcss())
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest(buildOutput + 'css'));
 }
+
+exports.html = gulp.series(_renderHtml('report'));
